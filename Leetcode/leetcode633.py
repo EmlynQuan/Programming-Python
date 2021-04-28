8b277b31ee650e8df602124e3e254a3698ab6f90
junyuanQuan <2569627904@qq.com>
2021-04-28 17:07:47 +0800

    leetcode

--- Leetcode/leetcode633.py
+++ Leetcode/leetcode633.py
@@ -1,1 +1,29 @@
 # coding=utf-8
+import math
+
+def judgeSquareSum(c):
+    '''
+    判断一个数是否可以拆成两个整数平方的和
+    :param c: 给定的数
+    :return: 是否可以分解 True / False
+    '''
+
+    temp = math.sqrt(c)
+    temp = (int)(math.ceil(temp))
+
+    # arr = [i for i in range(temp+1)]
+    left, right = 0, temp
+
+    while left <= right:
+        temp = left**2 + right**2
+        if temp == c:
+            return True
+        elif temp > c:
+            right -= 1
+        else:
+            left += 1
+
+    return False
+
+if __name__ == "__main__":
+    judgeSquareSum(5)
