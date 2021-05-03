0ffa1950b91aa3a9887bfc1dcd8705a1b5eb87a6
junyuanQuan <2569627904@qq.com>
2021-05-03 15:38:29 +0800

    leetcode

--- Leetcode/leetcode134.py
+++ Leetcode/leetcode134.py
@@ -1,3 +1,28 @@
 # coding=utf-8
 
-def canCompleteCircuit(self, gas, cost):
+def canCompleteCircuit(gas, cost):
+    n = len(gas)
+    # 索引
+    for idx in range(n):
+        sum = 0
+        flag = True
+        t = 0
+        while True:
+            # 跳出循环
+            if t >= n:
+                break
+            sum += gas[(t + idx) % n] - cost[(t + idx) % n]
+            if sum < 0:
+                flag = False
+                break
+            t += 1
+
+        if flag:
+            return idx
+    return -1
+
+
+if __name__ == "__main__":
+    gasArr = [1, 2, 3, 4, 5]
+    costArr = [3, 4, 5, 1, 2]
+    print canCompleteCircuit(gasArr, costArr)
---
+++ Leetcode/leetcode148.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def sortList(head):
+    '''
+    对链表排序
+    '''
+    return helpSort(head, None)
+
+
+def helpSort(head, tail):
+    # 头为空
+    if not head:
+        return head
+    # 只有一个节点
+    if head.next == tail:
+        head.next = None
+        return head
+    # 超过两个节点
+    slow = fast = head
+    while fast != tail:
+        slow = slow.next
+        fast = fast.next
+        if fast != tail:
+            fast = fast.next
+    mid = slow
+    return merge(helpSort(head, mid), helpSort(mid, tail))
+
+
+def merge(head1, head2):
+    ret = ListNode(0)
+    pos = ret
+    temp1, temp2 = head1, head2
+    while temp1 and temp2:
+        if temp1.val < temp2.val:
+            pos.next = head1
+            temp1 = temp1.next
+        else:
+            pos.next = temp2
+            temp2 = temp2.next
+        pos = pos.next
+    if temp1:
+        pos.next = temp1
+    if temp2:
+        pos.next = temp2
+    return ret.next
