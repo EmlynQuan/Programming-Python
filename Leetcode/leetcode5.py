0508f947168531c5f43341fbd66580228576b838
junyuanQuan <2569627904@qq.com>
2021-05-05 14:25:36 +0800

    leetcode

--- Leetcode/leetcode5.py
+++ Leetcode/leetcode5.py
@@ -4,10 +4,33 @@ def longestPalindrome(s):
     n = len(s)
     dp = [[0 for _ in range(n)] for _ in range(n)]
 
-    for i in range(n-1, 0, -1):
-        for j in range(i, -1, -1):
-            if i == j:
-                dp[i][j] = 1
-            else:
-                if dp[i][j] != 0 and j+1 < n and i-1 >= 0 and s[i-1] == s[j+1]:
-                    dp[i][j] = dp[][]
+    maxLen = 0
+    begin = -1
+    # 子串的长度
+    for length in range(1, n+1):
+        # 子串的起始位置
+        for i in range(0, n-length+1):
+            j = i + length - 1
+            # 如果两端的不相等 就不做处理 因为初始化已经为0
+            # 如果相等
+            # print s[i:j+1]
+            if s[i] == s[j]:
+                # 是同一个字母 或者是相邻两个
+                if j-i < 2:
+                    dp[i][j] = 1
+                    if length > maxLen:
+                        begin = i
+                        maxLen = length
+
+                # 子串是回文串 子串不是回文串不需要更新
+                elif dp[i+1][j-1] != 0:
+                    dp[i][j] = 1
+
+                    if length > maxLen:
+                        begin = i
+                        maxLen = length
+
+    return s[begin:begin+maxLen]
+
+if __name__ == "__main__":
+    print longestPalindrome("cbbd")
--- Leetcode/leetcode740.py
+++ Leetcode/leetcode740.py
@@ -1,1 +1,22 @@
 # coding=utf-8
+
+# 动态规划
+def deleteAndEarn(nums):
+    maxNum = max(nums)
+    mySum = [0 for _ in range(maxNum)]
+    for num in nums:
+        mySum[num-1] += num
+    dp = [0 for _ in range(maxNum)]
+
+    dp[0] = mySum[0]
+    if maxNum > 1:
+        dp[1] = max(dp[0], mySum[1])
+    for i in range(2, maxNum):
+        dp[i] = max(dp[i-2]+mySum[i], dp[i-1])
+    return dp[-1]
+
+
+
+if __name__ == "__main__":
+    nums = [1]
+    print deleteAndEarn(nums)
