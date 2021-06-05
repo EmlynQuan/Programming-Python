e5ef859ee26e415f27a78d12805f637040e34544
junyuanQuan <2569627904@qq.com>
2021-06-06 00:19:58 +0800

    leetcode

--- JZOffer/JZOffer60.py
+++ JZOffer/JZOffer60.py
@@ -9,14 +9,12 @@ class Solution(object):
         dp = [0.0 for _ in range(6*n+1)]
         dp[0] = 1
         self.dfs(dp, 1, n)
-        print dp[n:-1]
-        return dp[n:-1]
+        return dp[n:]
 
     def dfs(self, dp, curN, n):
         if curN == n+1:
             return
         else:
-            self.dfs(dp, curN+1, n)
             temp = [dp[i] for i in range(curN-1, 6*(curN-1)+1)]
             for i in range(curN, 6*curN+1):
                 dp[i] = 0
@@ -25,8 +23,9 @@ class Solution(object):
             for i in range(1,7):
                 # 遍历之前存在的可能取之的概率
                 for j in range(curN-1, 6*(curN-1)+1):
-                    dp[j+i] += temp[j]*1.0/6
-
+                    # print i, j, j+i
+                    dp[j+i] += temp[j-(curN-1)]*1.0/6
+            self.dfs(dp, curN+1, n)
 
 if __name__ == '__main__':
     Solution().dicesProbability(2)
---
+++ Leetcode/leetcode474.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
